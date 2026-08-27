ee077dabb0c669fa561e857e58466fc4e84ccc99
Gayatri-1603 <gayatri.tambe16@gmail.com>
2026-08-27 20:45:01 +0530

    .

--- panda 1.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np
-
-data = np.array(['10', '20', '30', '40', '50'])
-s = pd.Series(data)
-print("Pandas Series:")
-print(s)
--- squareroot.py
+++
@@ -1,5 +0,0 @@
-import math
-Num=(float(input("Enter the Number:\n")))
-sqrt=math.sqrt(Num)
-print("Square root of the number is:-\n",sqrt)
- 
